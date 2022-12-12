96599787d863437f9b488a8e55c91d1f5a412c9f
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2022-12-13 00:43:25 +0200

    registration show

--- RN01/App.js
+++ RN01/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, Image} from 'react-native';
+import { StyleSheet, View, ImageBackground, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import React, { useState, useCallback } from 'react';
+import React, { useCallback } from 'react';
 
 import RegistrationForm from './Screens/RegistrationScreen/RegistrationScreen';
 
@@ -11,6 +11,7 @@ SplashScreen.preventAutoHideAsync();
 const bgImg = require("./assets/images/bg-img-1x.jpg");
 
 export default function App() {
+
   const [fontsLoaded] = useFonts({
   "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
   "Roboto-Bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
@@ -28,11 +29,13 @@ export default function App() {
   
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
+      <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
       <ImageBackground source={bgImg} style={styles.image}>
-        <RegistrationForm />
-      </ImageBackground>
+            <RegistrationForm />
+        </ImageBackground>
+        </TouchableWithoutFeedback>
       <StatusBar style="auto" />
-    </View>
+      </View>
   );
 }
 
@@ -45,6 +48,6 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: "cover",
-    paddingTop: 235,
+    justifyContent: "flex-end",
   },
 });
--- RN01/Screens/RegistrationScreen/RegistrationScreen.js
+++ RN01/Screens/RegistrationScreen/RegistrationScreen.js
@@ -1,19 +1,39 @@
-import { StyleSheet, View, Image, Text, TextInput, TouchableOpacity} from 'react-native';
-import React, { useState } from 'react';
+import {
+    StyleSheet, View, Image, Text, TextInput, TouchableOpacity,
+    KeyboardAvoidingView, Platform, Keyboard, TouchableWithoutFeedback
+} from 'react-native';
+import React, { useCallback, useState } from 'react';
 
 const noAvatar = require("../../assets/images/no-avatar-1x.png");
 
 export default function RegistrationForm() {
+    const [login, setLogin] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState('');
+    const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+    const [isShown, setIsShown] = useState(true);
+
+    const keyboardHide = () => {
+        setIsShowKeyboard(false);
+        Keyboard.dismiss();
+    }
+
+ 
 
     return (
-        <View style={styles.formWrapper}>
-            <Image source={noAvatar} style={styles.noAvatar} />
-            <Text style={styles.title}>Registration</Text>
-            <View style={styles.formFields}>
-                <TextInput placeholder="Login" style={styles.formInput}></TextInput>
-                <TextInput placeholder="E-mail" style={styles.formInput}></TextInput>
-                <View>
+    <TouchableWithoutFeedback onPress={keyboardHide} onClick={keyboardHide}>
+            <View style={{ ...styles.formWrapper, paddingBottom: isShowKeyboard ? 0 : 45 }}>
+            <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+                <View style={{ ...styles.formWithAvatar, marginBottom: isShowKeyboard ? 0 : 16 }} >
+                    <Image source={noAvatar} style={styles.noAvatar} />
+                    <Text style={styles.title}>Registration</Text>
+                    <TextInput placeholder="Login" value={login} style={styles.formInput}
+                        onChangeText={text => setLogin(text)}
+                        onFocus={() => { setIsShowKeyboard(true) }} />
+                    <TextInput placeholder="E-mail" value={email} style={styles.formInput}
+                        onChangeText={text => setEmail(text)}
+                        onFocus={() => { setIsShowKeyboard(true) }} />
+            
                     <TextInput
                         placeholder="Password"
                         style={styles.formInput}
@@ -22,18 +42,20 @@ export default function RegistrationForm() {
                         value={password}
                         onChangeText={text => setPassword(text)}
                         enablesReturnKeyAutomatically
+                        onFocus={() => {setIsShowKeyboard(true)}}
                     />
-                    {/* <Pressable onPress={handlePasswordVisibility}>
-                            <Text value={rightText} style={styles.placeholder}></Text>
-                    </Pressable> */}
                 </View>
-
-                <TouchableOpacity style={styles.formBtn}>
-                    <Text style={styles.formBtnText}>Register now</Text>
-                </TouchableOpacity>
-            </View>
-            <Text style={styles.formText}>Do you have an account already? Log in</Text>
+                </KeyboardAvoidingView>
+                {!isShowKeyboard &&
+                    (<View isShown={isShown}>
+                <TouchableOpacity activeOpacity={0.8} style={{ ...styles.formBtn}} onPress={keyboardHide}>
+                <Text  style={styles.formBtnText}>Register now</Text>
+            </TouchableOpacity>
+                    <Text style={styles.formText}>Do you have an account already? Log in</Text>
+                    </View>)}
+                
         </View>
+   </TouchableWithoutFeedback>
     );
 };
 
@@ -44,13 +66,18 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 25,
     height: 549,
     paddingHorizontal: 16,
-  },
+    justifyContent: "flex-end",
+    },
+    formWithAvatar: {
+        marginBottom: 0,
+        backgroundColor: "transparent"
+    },
   noAvatar: {
     width: 120,
     height: 120,
-    background: "#F6F6F6",
+    backgroundColor: "#F6F6F6",
     borderRadius: 16,
-    paddingBottom: 32,
+      marginBottom: 32,
     alignSelf: "center",
   },
   title: {
@@ -59,13 +86,9 @@ const styles = StyleSheet.create({
     color: "#212121",
     textAlign: "center",
     lineHeight: 35,
-    letterSpacing: 0.01,
-      marginTop: 20,
-    paddingBottom: 33,
+      letterSpacing: 0.01,
+    marginBottom: 32,
     },
-    formFields: {
-        justifyContent: "center",
-  },
     formInput: {
         backgroundColor: "#F6F6F6",
         color: "#BDBDBD",
@@ -86,6 +109,7 @@ const styles = StyleSheet.create({
         borderRadius: 100,
         height: 51,
         padding: 16,
+        marginBottom: 16,
     },
     formBtnText: {
         color: "#fff",
@@ -97,7 +121,6 @@ const styles = StyleSheet.create({
         color: "#1B4371",
         fontSize: 16,
         lineHeight: 19,
-        marginTop: 16,
         paddingHorizontal: 20,
     }
 })
