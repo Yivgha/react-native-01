7bffce3ff4c6453d38fb67f8ee8ef62a76a80d51
quicklab <genieukne@gmail.com>
2023-08-20 21:15:07 +0300

    firebase auth

---
+++ RN01/src/firebase/firebaseConfig.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhk_H0_C_XKDMpRaoKy9aXAaNTP3k_iW8",
+  authDomain: "rn01-fe0d5.firebaseapp.com",
+  projectId: "rn01-fe0d5",
+  storageBucket: "rn01-fe0d5.appspot.com",
+  messagingSenderId: "650244899589",
+  appId: "1:650244899589:web:9f5c74f84b0b3847cb2d6c"
+};
+
+export default firebase.initializeApp(firebaseConfig);
+
+
