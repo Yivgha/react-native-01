b7cda109ae70b46a91348cd5528a256a7a1024e8
quicklab <genieukne@gmail.com>
2023-08-24 00:19:18 +0300

    save username

--- RN01/src/redux/auth/authReducer.js
+++ RN01/src/redux/auth/authReducer.js
@@ -4,8 +4,13 @@ export const authSlice = createSlice({
     name: "auth",
     initialState: {
         userId: null,
-        nickname: null
+        nickname: null,
+        stateChange: null,
     },
-    reducers: {}
+    reducers: {
+        updateUserProfile: (state, { payload }) => ({ ...state, userId: payload.userId, nickname: payload.nickname }),
+        authStateChange: (state, {payload}) => ({...state, stateChange: payload.stateChange})
+    }
 })
 
+// console.log("authSlice", authSlice);
